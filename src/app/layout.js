268ce6a6729f37ce7ffa2572bac1c5811dfdc627
Boyyys Dev <farhanaanking2004@gmail.com>
2025-04-24 16:48:10 +0700

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,23 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
+import Providers from './Providers';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'IMDb clone',  
-  description: 'This is movie database clone',
+  title: 'IMDb Clone',
+  description: 'A clone of the IMDb movie database.',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Header />
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
