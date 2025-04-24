836d5a2ac0aea84903f80c3ed99fe7a548d0efce
Boyyys Dev <farhanaanking2004@gmail.com>
2025-04-24 11:08:05 +0700

    Initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import MenuItem from './MenuItem';
 import React from 'react';
 import { AiFillHome } from 'react-icons/ai';
-import { BsFillinfoCircleFill } from 'react-icons/bs';
+import { BsFillInfoCircleFill } from 'react-icons/bs';
 
 export default function Header() {
   return (
     <div>
       <div className="">
         <MenuItem title="home" address="/" Icon={AiFillHome} />
-        <MenuItem title="about" address="/about" Icon={BsFillinfoCircleFill} />
+        <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
       <div className=""></div>
     </div>
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 export default function MenuItem({ title, address, Icon }) {
   return (
-    <Link href={address}>
-      {Icon && <Icon className="text-2xl sm:hidden" />}
+    <Link href={address} className="hover:text-amber-500">
+      <Icon className="text-2xl sm:hidden" />
       <p className="uppercase hidden sm:inline text-sm ">{title}</p>
     </Link>
   );
